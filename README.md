7a574f4c9789cd2dabd24ef5042bf6a4ff4d4ee6
Will Urbanski <will.urbanski@gmail.com>
2012-05-12 21:54:39 -0400

    Updated support files
    Updated the README and added an additional test case to the default
    signature set.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 urlmonitor
 ==========
 
-check URLs for PCREs
+This simple utility requests a URL and compares its source against a regular expression.
+
+output format:
+timestamp, domain, url, url content md5 hash, pcre, match text
 
 author: will urbanski
--- urlmonitor.py
+++ urlmonitor.py
@@ -1,34 +1,55 @@
 #!/usr/bin/python
+#
+#   urlmonitor
+#
+#   license: gnu gpl v3
+#   author: will urbanski
+#
 
 import urllib2
 import re
 import json
+import hashlib
+import time
+
 from pprint import pprint
+from urlparse import urlparse
+from optparse import OptionParser
+
+#the default path for the signature file
+file_path = "urls.json"
+
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="filename", help="read signatures from FILE", metavar="FILE")
+(options, args) = parser.parse_args()
+if (options.filename != None):
+    file_path = options.filename
 
 #load urls.json for monitoring
-json_data=open("urls.json").read()
+json_data=open(file_path).read()
 data = json.loads(json_data)
 
+domains = []
+urls = 0
+checks = 0
+matches = 0
+
 for item in data:
-    bOffending = False
-    print "\n"
     url = item['url']
     pcre = item['pcre']
-    print "Checking %s" % url
+    
+    oURL = urlparse(url)
+    domains.append(oURL.netloc)
+    urls += 1
     
     req = urllib2.Request(url)
     response = urllib2.urlopen(req)
     the_page = response.read()
+    md5sum = hashlib.md5()
+    md5sum.update(the_page)
     for s_pcre in pcre:
         m = re.search(s_pcre,the_page)
+        checks += 1
         if (m != None):
-            print "Match found: %s" % m.group(0)
-            bOffending = True
-        else:
-            print "Match not found: %s" % s_pcre
-    if (bOffending == True):
-        print " ++ URL is offending! ++"
-    else:
-        print " -- URL is clean --"
-    
-    #print the_page
+            print "%s,\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"" % (time.time(),oURL.netloc,url,md5sum.hexdigest(),s_pcre,m.group(0))
+            matches += 1
